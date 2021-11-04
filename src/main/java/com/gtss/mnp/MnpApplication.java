03deccef30bb74773925e5f7734eed66e38679cb
Assem Hassan <64113331+AssemALi94@users.noreply.github.com>
2021-11-04 18:31:57 +0200

    first commit

---
+++ src/main/java/com/gtss/mnp/MnpApplication.java
@@ -0,0 +1,13 @@
+package com.gtss.mnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtss/mnp/MnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtss.mnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
