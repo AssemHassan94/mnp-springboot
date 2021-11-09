2a492aaf39206c145dbd84daf63ce6959a5ba00f
Assem Hassan <64113331+AssemHassan94@users.noreply.github.com>
2021-11-10 00:06:29 +0200

    Update MnpApplication.java

--- src/main/java/com/gtss/mnp/MnpApplication.java
+++ src/main/java/com/gtss/mnp/MnpApplication.java
@@ -14,23 +14,4 @@ public class MnpApplication {
     public static void main(String[] args) {
         SpringApplication.run(MnpApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner run(OperatorService operatorService) {
-//        return args -> {
-////				userService.saveRole(new RoleDto(null, "ROLE_USER"));
-////				userService.saveRole(new RoleDto(null, "ROLE_MANAGER"));
-////				userService.saveRole(new RoleDto(null, "ROLE_ADMIN"));
-////				userService.saveRole(new RoleDto(null, "ROLE_SUPER_ADMIN"));
-////
-////				userService.saveUser(new UserDto(null, "Assem", "123456", "bio1"));
-////				userService.saveUser(new UserDto(null, "Ahmed", "123456", "bio2"));
-////				userService.saveUser(new UserDto(null, "MO", "123456", "bio3"));
-////				userService.saveUser(new UserDto(null, "MOSalah", "123456", "bio4"));
-////
-//            operatorService.createOperator(new OperatorDto(null, "vodafone", "010"));
-//            operatorService.createOperator(new OperatorDto(null, "etisalat", "011"));
-//            operatorService.createOperator(new OperatorDto(null, "orange", "012"));
-//        };
-//    }
 }
